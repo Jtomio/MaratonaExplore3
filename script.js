bccef1c11497a3358458cb92f2508bef788fa415
Jeison Tomio <medriase@gmail.com>
2022-08-10 06:21:17 -0300

    upload project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script simples para alterar o color do body em dark mode
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark-blue')
+});
